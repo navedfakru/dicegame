cb8316f204a21455fd82ee3be3e5a4f71066027a
Naved Ali <128811230+NavedGreat@users.noreply.github.com>
2023-12-13 08:30:25 +0530

    Update index.js
    var to let

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;//1-6 
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;//1-6 
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+let randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+let randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+let image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
  
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
